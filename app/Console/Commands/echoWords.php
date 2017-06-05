899c9e762a3568c8c1fd1cc288340793999d4b61
Artyombi <artyombi07@gmail.com>
2017-06-05 13:55:42 +0300

    New parser project on Laravel PHP framework

---
+++ app/Console/Commands/echoWords.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class echoWords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'words:echo {string}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show words in console';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+	    $this->line('Display this on the screen');
+    }
+}
